396cdbbe92450d275d56fcc7289992b4b148b555
Caroline Cox <carolinecox@carolines-air-5.dyn.uncc.edu>
2023-06-21 11:20:28 -0400

    new jobs

--- transport_slurm_gen.py
+++ transport_slurm_gen.py
@@ -2,14 +2,14 @@
 graph_types = [0, 1, 2, 3]
 
 graph_params = {
-    0: list(range(3,21)),
-    1: list(range(3,21)),
-    2: list(range(4,22,2)),
-    3: list(range(2,5))
+    0: list(range(12,21)),
+    1: list(range(12,21)),
+    2: list(range(14,22,2)),
+    3: list(range(4,5))
 }
 
-num_trials = 1000000000
-batch_size = 10000000
+num_trials = 10000000
+batch_size = 100000
 num_batches = num_trials//batch_size
 
 
@@ -24,7 +24,7 @@ for gtype in graph_types:
 #SBATCH --job-name={jname}
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 
 module load anaconda3
 
